8cea974ec854383554525ce96da54b224973c0b0
Richard Esplin <richard.esplin@alfresco.com>
2013-07-24 02:02:27 -0600

    Rolling back to Django 1.4 to see if that works.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Tim Austen, Eileen Lin, Richard Esplin',
     author_email='richard-oss@esplins.org',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.5',
+    install_requires=['Django<=1.4',
                       #  'MySQL-python',
                       #  'pymongo',
                       #  'psycopg2',
