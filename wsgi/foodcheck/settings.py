4fd07c2e05940c83d76bda6c5ac2b6d7e9da559b
tjausten <austentj@gmail.com>
2013-07-29 16:44:20 -0700

    settings.py updated to connect PostgreSQL

--- wsgi/foodcheck/settings.py
+++ wsgi/foodcheck/settings.py
@@ -24,6 +24,17 @@ if ON_OPENSHIFT:
     # with rhc cartridge add (see /README in this git repo)
     DATABASES = {
         'default': {
+            'ENGINE': 'django.db.backends.postgresql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'live',  # Or path to database file if using sqlite3.
+            'USER': 'admingtayl7h',                      # Not used with sqlite3.
+            'PASSWORD': ' Vhi6E4p2qELM',                  # Not used with sqlite3.
+            'HOST': 'postgresql://$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT',  # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }    
+else:
+   DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'sqlite3.db'),  # Or path to database file if using sqlite3.
             'USER': '',                      # Not used with sqlite3.
@@ -32,18 +43,6 @@ if ON_OPENSHIFT:
             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': os.path.join(PROJECT_DIR, 'sqlite3.db'),  # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-    }
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
