b1ad6a6afca8f7a00f5aa47ec8ba94f679422d41
Richard Esplin <richard.esplin@alfresco.com>
2013-07-29 16:03:34 -0600

    Correctly deploy static files.

--- wsgi/foodcheck/settings.py
+++ wsgi/foodcheck/settings.py
@@ -93,9 +93,11 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+# Listing the project dir here avoids having to collect static files in a 
+# subdirectory i.e. /static/css instead of /static/foodcheck/css
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
