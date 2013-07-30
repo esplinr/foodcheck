e29cc7c6cd8a623dac871f30476ba9a2fa4227e6
Richard Esplin <richard.esplin@alfresco.com>
2013-07-29 20:13:56 -0600

    Merging Eileen's changes with my previous changes (mostly style)

--- data_dumps/food_check.py
+++ data_dumps/food_check.py
@@ -4,17 +4,14 @@ import os.path, time
 import urllib2
 
 
-
 if len(sys.argv) < 2:
     print("Please enter a restaurant name.")
     search_1 = input("Search for:")
 else:
     search_1=sys.argv[1]
 
-
 ## check whether we need to update the file
 time_ori=os.path.getmtime(file)
-
 
 sf_food=urlib2.request.urlopen("https://204.68.210.15/food/SFFoodProgram_Complete_Data.zip")
 time_now = sf_food.info().getdate('last-modified')
@@ -25,18 +22,13 @@ if time_now > time_ori
    sf_data.close()
 
 
-
 ## read variables for searching
-
 cvsfile=open("business_id.csv","r")
 cvsfile.seek
 reader=csv.reader(csvfile,dialect='excel', delimiter=",")
 
 ## search for the restaurant
-
 for line in reader:
-
-
    if search_1 in line:
        business_id=line[1];
        latitude=line[7];
@@ -48,11 +40,9 @@ cvsfile=open("business_id.csv","r")
 cvsfile.seek
 reader=csv.reader(csvfile,dialect='excel', delimiter=",")
 
+
 ## search for the score
-
 for line in reader:
-
-
    if business_id in line:
       score=line[2]
 
@@ -60,4 +50,4 @@ for line in reader:
 csvfile.close()
 
 
-
+#vim tabstop=8 expandtab shiftwidth=4 softtabstop=4
