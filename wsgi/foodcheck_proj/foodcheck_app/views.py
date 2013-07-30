340f20f565137d2ceb27f82f73442c8013dec4b8
Richard Esplin <richard.esplin@alfresco.com>
2013-07-30 01:53:30 -0600

    Trying to get it to see the template

--- wsgi/foodcheck_proj/foodcheck_app/views.py
+++ wsgi/foodcheck_proj/foodcheck_app/views.py
@@ -2,7 +2,7 @@ import os
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'foodcheck/home.html',
+    return render(request, 'home.html',
                   {},
                  )
 
