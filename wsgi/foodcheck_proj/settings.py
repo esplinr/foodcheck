f168f8c6b57abeffe1c2ea5b55dc25e6283b94fb
Richard Esplin <richard.esplin@alfresco.com>
2013-07-30 22:27:03 -0600

    Testing upgraded leaflet.

--- wsgi/foodcheck_proj/settings.py
+++ wsgi/foodcheck_proj/settings.py
@@ -152,7 +152,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-#    'leaflet',
+    'leaflet',
     'foodcheck_app', 
 )
 
