083692b4b2858a96978df6d11f19f2999e8c142a
System Administrator <root@rescomp-10-174707.stanford.edu>
2013-08-04 13:28:23 -0700

    add data type

--- wsgi/foodcheck_proj/foodcheck_app/models.py
+++ wsgi/foodcheck_proj/foodcheck_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+  id=models.IntegerField()
+  name=models.CharField(max_length=40)
+  address=models.CharField(max_length=50)
+  city=models.CharField(max_length=50)
+  state=models.CharField(max_length=50)
+  postal_code=models.IntegerField()
+  latitude=models.DecimalField()
+  longitude=models.DecimalField()
+  phone_no=models.IntegerField()
+
+class Score(models.Model):
+  id=models.IntegerField()
+  date=models.DateField()
+  score=models.IntegerField()
+  type=models.CharField(max_length=50)
+
+
+class Violation(models.Model)
+   id=models.IntegerField()
+   date=models.DateField()
+   vi_type=models.CharField(max_length=20)
+   vi_severe=models.CharField(max_length=20)
+   vi_description=models.CharField(max_length=50)
+
+
+~
+~
+~
+~
+# Create your models here.
