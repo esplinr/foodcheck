13260e110fad3bd61732f83624e52feb554ac67f
Richard Esplin <richard.esplin@alfresco.com>
2013-08-09 21:50:35 -0600

    Forgot to seperate parameters with commas.

--- wsgi/foodcheck_proj/foodcheck_app/models.py
+++ wsgi/foodcheck_proj/foodcheck_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Restaurant(models.Model):
     city_business_id=models.IntegerField(
-        verbose_name="business ID for city"
+        verbose_name="business ID for city",
         help_text="The ID the city uses for this business")
     name=models.CharField(max_length=40)
     address=models.CharField(max_length=50)
@@ -17,14 +17,14 @@ class Restaurant(models.Model):
 
 class Inspection(models.Model):
     restaurant = models.ForeignKey(Restaurant,
-        verbose_name="related restaurant", on_delete=models.CASCADE
+        verbose_name="related restaurant", on_delete=models.CASCADE,
         help_text="Connect to the restaurant by the city_business_id")
     city_business_id=models.IntegerField(
         help_text="City business ID, used to match to Restaurant")
     date=models.DateField()
     score=models.IntegerField()
     reason=models.CharField(verbose_name="Reason for Inspection",
-        max_length=50
+        max_length=50,
         help_text="The reason for the inspection is given in the inspection type")
 
 
@@ -33,7 +33,6 @@ class Violation(models.Model):
         verbose_name="inspection during which violation was found",
         on_delete=models.CASCADE,
         help_text="Connect to the inspection by the city_business_id + date")
-    # TODO -- should foreign key be Inspection or Restaruant?
     city_business_id=models.IntegerField(
         help_text="City business ID, used to match to Restaurant")
     date=models.DateField(verbose_name="date of violation")
