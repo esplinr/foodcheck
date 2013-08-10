112ed3fc31930955099ee2767f2709c3c612c1b1
Richard Esplin <richard.esplin@alfresco.com>
2013-08-10 10:15:59 -0600

    Add copyright and license notice

--- wsgi/foodcheck_proj/foodcheck_app/management/commands/import_city_data.py
+++ wsgi/foodcheck_proj/foodcheck_app/management/commands/import_city_data.py
@@ -1,7 +1,27 @@
+'''
+Custom django admin command to import data from CSV file into database
+'''
+#    Copyright (C) 2013 Timothy James Austen, Eileen Qiuhua Lin,
+#                       Richard Esplin <richard-oss@esplins.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import csv
 from django.core.management.base import BaseCommand
 from foodcheck_app.models import Restaurant, Inspection, Violation
+
 
 class Command(BaseCommand):
 #    args = '<city_name city_name ...>' #Don't know what this does yet
