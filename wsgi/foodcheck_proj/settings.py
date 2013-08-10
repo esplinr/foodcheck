0d4724bfdc31a24d5735310b9a195ee41c029736
Richard Esplin <richard.esplin@alfresco.com>
2013-08-10 10:16:27 -0600

    Add copyright and license notices

--- wsgi/foodcheck_proj/settings.py
+++ wsgi/foodcheck_proj/settings.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
-# Django settings for foodcheck project.
+'''
+Django settings for foodcheck project
+'''
+#    Copyright (C) 2013 Timothy James Austen, Eileen Qiuhua Lin,
+#                       Richard Esplin <richard-oss@esplins.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import imp, os
+
 
 # a setting to determine whether we are running on OpenShift
 ON_OPENSHIFT = False
