dbd3f03716e63546d623e1eb74a5e2c919ff66db
Richard Esplin <richard.esplin@alfresco.com>
2013-08-10 10:05:13 -0600

    Remove functionality from download script that is now in import script.

--- util/download_data_sf.py
+++ util/download_data_sf.py
@@ -22,14 +22,13 @@ Dowload San Francisco health inspector data.
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-if len(sys.argv) < 2:
-    print("Please enter a restaurant name.")
-    search_1 = input("Search for:")
-else:
-    search_1=sys.argv[1]
+#TODO Detect most recent previous downloaded file
+#TODO Fix file layout: if __name__ == "__main__":
+
+last_zip_file = "" # Insert path here
 
 ## check whether we need to update the file
-time_ori=os.path.getmtime(file)
+time_ori=os.path.getmtime(last_zip_file)
 
 sf_food=urlib2.request.urlopen("https://204.68.210.15/food/SFFoodProgram_Complete_Data.zip")
 time_now = sf_food.info().getdate('last-modified')
@@ -38,36 +37,6 @@ if time_now > time_ori
    sf_data=open("SFFoodProgram_Complete_Data.zip","w")
    sf_data.write(sf_food.read())
    sf_data.close()
-
-
-## read variables for searching
-cvsfile=open("business_id.csv","r")
-cvsfile.seek
-reader=csv.reader(csvfile,dialect='excel', delimiter=",")
-
-## search for the restaurant
-for line in reader:
-   if search_1 in line:
-       business_id=line[1];
-       latitude=line[7];
-       longitude=line[8];
-
-
-## read the score
-cvsfile=open("inspection_plus.csv","r")
-cvsfile.seek
-reader=csv.reader(csvfile,dialect='excel', delimiter=",")
-
-
-## search for the score
-for line in reader:
-   if business_id in line:
-      score=line[2]
-      time=line[3]
-      vilation=line[4]
-
-
-csvfile.close()
 
 
 #vim tabstop=8 expandtab shiftwidth=4 softtabstop=4
