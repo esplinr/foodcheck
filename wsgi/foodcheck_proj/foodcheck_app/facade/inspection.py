d08803798d7ba5f208ac39cee75f0e59a1ebd7e6
Richard Esplin <richard.esplin@alfresco.com>
2013-08-12 16:47:26 -0600

    Minor space fix.

--- wsgi/foodcheck_proj/foodcheck_app/facade/inspection.py
+++ wsgi/foodcheck_proj/foodcheck_app/facade/inspection.py
@@ -98,7 +98,7 @@ class Inspection():
 
 
     def load_violations(self):
-       self.violations=load_violations(self) 
+        self.violations=load_violations(self) 
 
 
 # vim:expandtab tabstop=8 shiftwidth=4 ts=8 sw=4 softtabstop=4
