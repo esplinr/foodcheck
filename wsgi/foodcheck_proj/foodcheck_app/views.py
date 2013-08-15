89ebafd6fe9cef74e90fae57fdfcb24caf72555a
tjausten <austentj@gmail.com>
2013-08-15 05:23:26 -0700

    Getting dev working again

--- wsgi/foodcheck_proj/foodcheck_app/views.py
+++ wsgi/foodcheck_proj/foodcheck_app/views.py
@@ -1,27 +1,27 @@
 '''
 Django View functions for foodcheck_app
 '''
-#    Copyright (C) 2013 Timothy James Austen, Eileen Qiuhua Lin,
-#                       Richard Esplin <richard-oss@esplins.org>
+# Copyright (C) 2013 Timothy James Austen, Eileen Qiuhua Lin,
+# Richard Esplin <richard-oss@esplins.org>
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 import os
 from django.shortcuts import render
 from django.http import HttpResponse
-import facade 
+import facade
 
 def home(request):
     businesses = facade.load_businesses_by_name('Cafe', no_details=True)
@@ -31,15 +31,12 @@ def home(request):
 
 def about(request):
 	return render(request, 'about.html',)
-	
-# def about(request):
-#	return render(request, 'resources.html',)
-	
+
 def search(request):
     if 'q' in request.GET:
-       message = 'You searched for: %s' % request.GET['q']
+        message = 'You searched for: %s' % request.GET['q']
         # GET['q'] contains a db_id for the business
-        businesses = facade.Business(request.GET)
+        business = facade.Business(request.GET)
     else:
         message = 'You submitted an empty form.'
     return render(request, 'home.html', {'error': True})
@@ -47,3 +44,6 @@ def search(request):
 
 
 # vim:expandtab tabstop=8 shiftwidth=4 ts=8 sw=4 softtabstop=4
+
+
+	
